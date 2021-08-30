19f01cad470d25a0e33340a91cda91ab5aa6ca61
MarharytaBoichenko <boichenkomv@gmail.com>
2021-08-30 20:45:52 +0300

    add  refs file

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,13 @@
 import '../sass/main.scss';
+import menu from "../menu.json";
+console.log(menu);
+import markup from "../templates/markup.hbs";
 
 import { refs } from "../js/refs.js";
-// const { menuEl } = refs;
-
-const menuEl = document.querySelector(".js-menu");
-const inputEl = document.querySelector("#theme-switch-toggle");
-const body = document.querySelector("body")
-
-// console.log(refs.menuEl);
-// console.log(refs.inputEl);
-
+const { menuEl, inputEl, body } = refs;
+console.log(menuEl);
+console.log(inputEl);
+console.log(body);
 
 const Theme = {
   LIGHT: 'light-theme',
@@ -18,18 +16,10 @@ const Theme = {
 
 const {LIGHT, DARK} = Theme
 
-import menu from "../menu.json";
-console.log(menu);
-
-import markup from "../templates/markup.hbs";
-
   const menuMarkup = markup(menu);
   console.log(menuMarkup);
   menuEl.insertAdjacentHTML("afterbegin", menuMarkup)
-
-
 inputEl.addEventListener("change", changeTheme);
-// inputEl.addEventListener("change", setToLS);
 
 function changeTheme() {
   if (inputEl.checked) {
@@ -42,15 +32,6 @@ function changeTheme() {
      localStorage.setItem("class", LIGHT);
   }
 }
-
-// function setToLS(theme, dark) {
-//   if (inputEl.checked) {
-//     console.log(inputEl.checked);
-//     localStorage.setItem("class", "dark");
-//   } else {
-//     localStorage.setItem("class", "light");
-//   }
-// }
 
   const currentTheme = localStorage.getItem("class");
   console.log(currentTheme);
--- src/js/refs.js
+++ src/js/refs.js
@@ -1,7 +1,7 @@
 export const refs =  {
     menuEl: document.querySelector(".js-menu"),
-    inputEl: document.querySelector("#theme-switch-toggle")
-  
+inputEl: document.querySelector("#theme-switch-toggle"),
+ body: document.querySelector("body"),
 }
-// console.log(menuEl);
-// console.log(inputEl);
+// console.log(refs.menuEl);
+// console.log(refs.inputEl);
